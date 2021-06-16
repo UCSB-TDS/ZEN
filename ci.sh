88a69119c65dd206f84277153c618daa22713440
AnonymousRobotics <lianke.qin@gmail.com>
2021-06-16 15:26:04 -0700

    fix ci

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@ build_test(){
   exit_code=0
   while read path; do
     printf "Project: %s\n" "$path"
-    rustup overide set 1.47.0
+    rustup override set 1.47.0
     cargo build --verbose --manifest-path "$path" || exit_code=1
     #cargo test takes too much time to complete on small VM
     #cargo test --verbose --manifest-path "$path" || exit_code=1
