8b498c6a4d0dbb0f614ce4c74b5b408b02cd8281
Lianke Qin <lianke.qin@gmail.com>
2021-09-10 00:34:57 +0000

    upload benchmark new results

--- zen-arkworks/run.sh
+++ zen-arkworks/run.sh
@@ -1,12 +1,12 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt-get update
-sudo apt-get install cargo
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# sudo apt-get update
+# sudo apt-get install cargo
 
 
 
-cargo run --example shallownet_poseidon --release 
-cargo run --example lenet_small_cifar_poseidon --release 
-cargo run --example lenet_medium_cifar_poseidon --release
-cargo run --example lenet_small_face_poseidon --release
-cargo run --example lenet_medium_face_poseidon --release
-cargo run --example lenet_large_face_poseidon --release 
+cargo run --example shallownet_poseidon --release > shallownet.log
+cargo run --example lenet_small_cifar_poseidon --release > lenet_small_cifar.log
+cargo run --example lenet_medium_cifar_poseidon --release > lenet_medium_cifar.log
+cargo run --example lenet_small_face_poseidon --release > lenet_small_face.log
+cargo run --example lenet_medium_face_poseidon --release > lenet_medium_face.log
+cargo run --example lenet_large_face_poseidon --release > lenet_large_face.log
